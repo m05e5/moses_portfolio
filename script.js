c356f64470037dcfe6815bae3eef5d48d9f6a624
m05e5 <calebmoses1111@gmail.com>
2021-06-23 11:07:06 +0100

    implemented code reviewer sudjestions

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const menulink = document.querySelectorAll('nav ul li a');
 const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', () => {
   nav.classList.toggle('slide');
-  console.log('hello');
 });
 menulink.forEach((link) => link.addEventListener('click', () => {
   nav.classList.remove('slide');
