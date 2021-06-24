1ad90e3835fe5b706d3b57b5e70ce200d037d04a
m05e5 <calebmoses1111@gmail.com>
2021-06-24 17:47:26 +0100

    projects section of the index.html created dynamically from js

--- script.js
+++ script.js
@@ -16,164 +16,165 @@ menulink.forEach((link) => link.addEventListener('click', () => {
 }));
 
 // js modal object------------------------------------
-const card1 = {
+const cards =  [ {
+  key: 1,
+  id: 'c-img1',
   title: 'Tonic',
+  company_name: 'FACEBOOK',
+  company_type: 'Back End Dev',
+  company_year: '2015',
   description: ' Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
   img: './assets/Snapshoot1.png',
-  tag1: 'html',
-  tag2: 'Ruby on rails',
-  tag3: 'css',
-  tag4: 'javaScript',
-  githubLink: 'https://github.com/m05e5/moses_portfolio/',
-  liveDemoLink: 'https://m05e5.github.io/moses_portfolio/',
-};
-const card2 = {
+  alt: 'Card image',
+  tags: ['html', 'Ruby on Rails', 'css', 'javascript'],
+  githubLink: 'github.com/m05e5/moses_portfolio/',
+  liveDemoLink: 'www.google.com',
+},
+{
+  key: 2,
+  id: 'c-img2',
   title: 'Multi-Post Sorties',
+  company_name: 'FACEBOOK',
+  company_type: 'Back End Dev',
+  company_year: '2015',
   description: ' Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
   img: './assets/Snapshoot2.png',
-  tag1: 'html',
-  tag2: 'Ruby on rails',
-  tag3: 'css',
-  tag4: 'javaScript',
-  githubLink: 'https://github.com/m05e5/moses_portfolio/',
-  liveDemoLink: 'https://m05e5.github.io/moses_portfolio/',
-};
-const card3 = {
+  alt: 'Card image',
+  tags: ['html', 'Ruby on Rails', 'css', 'javascript'],
+  githubLink: 'github.com/m05e5/moses_portfolio/',
+  liveDemoLink: 'www.google.com',
+},
+{
+  key: 3,
+  id: 'c-img3',
   title: 'Facebook 360',
+  company_name: 'FACEBOOK',
+  company_type: 'Back End Dev',
+  company_year: '2015',
   description: ' Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
   img: './assets/Snapshoot3.png',
-  tag1: 'html',
-  tag2: 'Ruby on rails',
-  tag3: 'css',
-  tag4: 'javaScript',
-  githubLink: 'https://github.com/m05e5/moses_portfolio/',
-  liveDemoLink: 'https://m05e5.github.io/moses_portfolio/',
-};
-const card4 = {
+  alt: 'Card image',
+  tags: ['html', 'Ruby on Rails', 'css', 'javascript'],
+  githubLink: 'github.com/m05e5/moses_portfolio/',
+  liveDemoLink: 'www.google.com',
+},
+{
+  key: 4,
+  id: 'c-img4',
   title: 'Uber Navigator',
+  company_name: 'FACEBOOK',
+  company_type: 'Back End Dev',
+  company_year: '2015',
   description: ' Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
   img: './assets/Snapshoot4.png',
-  tag1: 'html',
-  tag2: 'Ruby on rails',
-  tag3: 'css',
-  tag4: 'javaScript',
-  githubLink: 'https://github.com/m05e5/moses_portfolio/',
-  liveDemoLink: 'https://m05e5.github.io/moses_portfolio/',
-};
+  alt: 'Card image',
+  tags: ['html', 'Ruby on Rails', 'css', 'javascript'],
+  githubLink: 'github.com/m05e5/moses_portfolio/',
+  liveDemoLink: 'www.google.com',
+}];
 
-const card1Title = document.querySelector('#card1 h2');
-const card1Img = document.querySelector('#card1 #c-img1');
-const card1Desc = document.querySelector('#card1 .description');
-const card1tag1 = document.querySelector('#card1 .tag1');
-const card1tag2 = document.querySelector('#card1 .tag2');
-const card1tag3 = document.querySelector('#card1 .tag3');
-const card1tag4 = document.querySelector('#card1 .tag4');
+const CardWrap = document.querySelector('#card-section');
+const lunchCards = () => {
+  cards.forEach((item) => {
+    // CREATE PROJECT ITEM
+    const list = document.createElement('li');
+    list.classList.add('grid-card');
+    // CREATE PROJECT IMAGE
+    const workImg = document.createElement('img');
+    workImg.classList.add('card-img');
+    if (item.key % 2 === 0) {
+      workImg.classList.add('c-2');
+    }
+    workImg.src = item.img;
+    workImg.alt = item.alt;
+    workImg.id = item.id;
+    list.appendChild(workImg);
+    const workbody = document.createElement('div');
+    workbody.classList.add('card-body');
+    // CREATE PROJECT HEADING
+    const cardHead = document.createElement('h2');
+    cardHead.classList.add('card-title');
+    cardHead.innerText = item.title;
+    workbody.appendChild(cardHead);
+    // CREATE PROJECT ATTRIBUTES
+    const workatt = document.createElement('div');
+    workatt.classList.add('card-attribut');
+    // CREATE PROJECT ATTRIBUTES COMPANY NAME
+    const companyName = document.createElement('span');
+    companyName.classList.add('att1');
+    companyName.innerText = item.company_name;
+    workatt.appendChild(companyName);
+    // CREATE PROJECT DETAILS-DOT
+    const dotImg = document.createElement('img');
+    dotImg.classList.add('dot');
+    dotImg.src = './assets/Counter.png';
+    dotImg.alt = 'dot';
+    workatt.appendChild(dotImg);
+    // CREATE PROJECT ATTRIBUTES COMPANY TYPE
+    const companyType = document.createElement('span');
+    companyType.classList.add('att2');
+    companyType.innerText = item.company_name;
+    workatt.appendChild(companyType);
+    // CREATE PROJECT DETAILS-SECOND-DOT
+    const secDotImg = document.createElement('img');
+    secDotImg.classList.add('dot');
+    secDotImg.src = './assets/Counter.png';
+    secDotImg.alt = 'dot';
+    workatt.appendChild(secDotImg);
+    // CREATE PROJECT ATTRIBUTES COMPANY DATE
+    const companyDate = document.createElement('span');
+    companyDate.classList.add('att2');
+    companyDate.innerText = item.company_name;
+    workatt.appendChild(companyDate);
+    workbody.appendChild(workatt);
 
-const card2Title = document.querySelector('#card2 h2');
-const card2Img = document.querySelector('#card2 #c-img2');
-const card2Desc = document.querySelector('#card2 .description');
-const card2tag1 = document.querySelector('#card2 .tag1');
-const card2tag2 = document.querySelector('#card2 .tag2');
-const card2tag3 = document.querySelector('#card2 .tag3');
-const card2tag4 = document.querySelector('#card2 .tag4');
+    // CREATE PROJECT DESCRIPTION
+    const workDescription = document.createElement('p');
+    workDescription.classList.add('card-text');
+    workDescription.innerText = item.description;
+    workbody.appendChild(workDescription);
+    // CREATE PROJECT TECHNOLOGIES LIST
+    const projectTags = document.createElement('ul');
+    projectTags.classList.add('tag-section');
+    item.tags.forEach((tech) => {
+      const techItem = document.createElement('li');
+      techItem.classList.add('tag-div');
+      const tag = document.createElement('span');
+      tag.classList.add('tag');
+      tag.innerText = tech;
+      techItem.appendChild(tag);
+      projectTags.appendChild(techItem);
+    });
+    workbody.appendChild(projectTags);
+    // CREATE SEE PROJECT BUTTON
+    const seeMoreBtn = document.createElement('button');
+    seeMoreBtn.type = 'button';
+    seeMoreBtn.classList.add('button');
+    seeMoreBtn.innerText = 'See Project';
+    seeMoreBtn.dataset.key = item.key;
+    seeMoreBtn.id = 'myBtn'+ item.key
+    workbody.appendChild(seeMoreBtn);
+    list.appendChild(workbody);
+    CardWrap.appendChild(list);
+  });
+}
 
-const card3Title = document.querySelector('#card3 h2');
-const card3Img = document.querySelector('#card3 #c-img3');
-const card3Desc = document.querySelector('#card3 .description');
-const card3tag1 = document.querySelector('#card3 .tag1');
-const card3tag2 = document.querySelector('#card3 .tag2');
-const card3tag3 = document.querySelector('#card3 .tag3');
-const card3tag4 = document.querySelector('#card3 .tag4');
-
-const card4Title = document.querySelector('#card4 h2');
-const card4Img = document.querySelector('#card4 #c-img4');
-const card4Desc = document.querySelector('#card4 .description');
-const card4tag1 = document.querySelector('#card4 .tag1');
-const card4tag2 = document.querySelector('#card4 .tag2');
-const card4tag3 = document.querySelector('#card4 .tag3');
-const card4tag4 = document.querySelector('#card4 .tag4');
-
-window.addEventListener('load', () => {
-  card1Title.textContent = card1.title;
-  card1Desc.textContent = card1.description;
-  card1Img.src = card1.img;
-  card1tag1.textContent = card1.tag1;
-  card1tag2.textContent = card1.tag2;
-  card1tag3.textContent = card1.tag3;
-  card1tag4.textContent = card1.tag4;
-
-  card2Title.textContent = card2.title;
-  card2Desc.textContent = card2.description;
-  card2Img.src = card2.img;
-  card2tag1.textContent = card2.tag1;
-  card2tag2.textContent = card2.tag2;
-  card2tag3.textContent = card2.tag3;
-  card2tag4.textContent = card2.tag4;
-
-  card3Title.textContent = card3.title;
-  card3Desc.textContent = card3.description;
-  card3Img.src = card3.img;
-  card3tag1.textContent = card3.tag1;
-  card3tag2.textContent = card3.tag2;
-  card3tag3.textContent = card3.tag3;
-  card3tag4.textContent = card3.tag4;
-
-  card4Title.textContent = card4.title;
-  card4Desc.textContent = card4.description;
-  card4Img.src = card4.img;
-  card4tag1.textContent = card4.tag1;
-  card4tag2.textContent = card4.tag2;
-  card4tag3.textContent = card4.tag3;
-  card4tag4.textContent = card4.tag4;
-});
+lunchCards();
 
 // js of my modal content------------------------------------
-const modal1Title = document.querySelector('#myModal1 h2');
-const modal1Img = document.querySelector('#myModal1 #pic1');
-const modal1Desc = document.querySelector('#myModal1 .description');
-const modal1tag1 = document.querySelector('#myModal1 .tag1');
-const modal1tag2 = document.querySelector('#myModal1 .tag2');
-const modal1tag3 = document.querySelector('#myModal1 .tag3');
-const modal1tag4 = document.querySelector('#myModal1 .tag4');
-const modal1live = document.querySelector('#myModal1 #live1');
-const modal1github = document.querySelector('#myModal1 #github1');
-
-const modal2Title = document.querySelector('#myModal2 h2');
-const modal2Img = document.querySelector('#myModal2 #pic2');
-const modal2Desc = document.querySelector('#myModal2 .description');
-const modal2tag1 = document.querySelector('#myModal2 .tag1');
-const modal2tag2 = document.querySelector('#myModal2 .tag2');
-const modal2tag3 = document.querySelector('#myModal2 .tag3');
-const modal2tag4 = document.querySelector('#myModal2 .tag4');
-const modal2live = document.querySelector('#myModal2 #live2');
-const modal2github = document.querySelector('#myModal2 #github2');
-
-const modal3Title = document.querySelector('#myModal3 h2');
-const modal3Img = document.querySelector('#myModal3 #pic3');
-const modal3Desc = document.querySelector('#myModal3 .description');
-const modal3tag1 = document.querySelector('#myModal3 .tag1');
-const modal3tag2 = document.querySelector('#myModal3 .tag2');
-const modal3tag3 = document.querySelector('#myModal3 .tag3');
-const modal3tag4 = document.querySelector('#myModal3 .tag4');
-const modal3live = document.querySelector('#myModal3 #live3');
-const modal3github = document.querySelector('#myModal3 #github3');
-
-const modal4Title = document.querySelector('#myModal4 h2');
-const modal4Img = document.querySelector('#myModal4 #pic4');
-const modal4Desc = document.querySelector('#myModal4 .description');
-const modal4tag1 = document.querySelector('#myModal4 .tag1');
-const modal4tag2 = document.querySelector('#myModal4 .tag2');
-const modal4tag3 = document.querySelector('#myModal4 .tag3');
-const modal4tag4 = document.querySelector('#myModal4 .tag4');
-const modal4live = document.querySelector('#myModal4 #live4');
-const modal4github = document.querySelector('#myModal4 #github4');
+const modalTitle = document.querySelector('#myModal h2');
+const modalImg = document.querySelector('#myModal #pic1');
+const modalDesc = document.querySelector('#myModal .description');
+const modaltag1 = document.querySelector('#myModal .tag1');
+const modaltag2 = document.querySelector('#myModal .tag2');
+const modaltag3 = document.querySelector('#myModal .tag3');
+const modaltag4 = document.querySelector('#myModal .tag4');
+const modallive = document.querySelector('#myModal #live1');
+const modalgithub = document.querySelector('#myModal #github1');
 
 // js of my modal------------------------------------
 // Get the modal
-const modal1 = document.getElementById('myModal1');
-const modal2 = document.getElementById('myModal2');
-const modal3 = document.getElementById('myModal3');
-const modal4 = document.getElementById('myModal4');
+const modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
 const btn1 = document.getElementById('myBtn1');
@@ -182,90 +183,69 @@ const btn3 = document.getElementById('myBtn3');
 const btn4 = document.getElementById('myBtn4');
 
 // Get the <span> element that closes the modal
-const span1 = document.getElementsByClassName('close')[0];
-const span2 = document.getElementsByClassName('close')[1];
-const span3 = document.getElementsByClassName('close')[2];
-const span4 = document.getElementsByClassName('close')[3];
+const span = document.getElementsByClassName('close')[0];
 
 // When the user clicks on the button, open the modal
 btn1.onclick = function () {
-  modal1.style.display = 'block';
-  modal1Title.textContent = card1.title;
-  modal1Desc.textContent = card1.description;
-  modal1Img.src = card1.img;
-  modal1tag1.textContent = card1.tag1;
-  modal1tag2.textContent = card1.tag2;
-  modal1tag3.textContent = card1.tag3;
-  modal1tag4.textContent = card1.tag4;
-  modal1live.href = card1.liveDemoLink;
-  modal1github.href = card1.githubLink;
+  modal.style.display = 'block';
+  modalTitle.textContent = cards[0].title;
+  modalDesc.textContent = cards[0].description;
+  modalImg.src = cards[0].img;
+  modaltag1.textContent = cards[0].tag1;
+  modaltag2.textContent = cards[0].tag2;
+  modaltag3.textContent = cards[0].tag3;
+  modaltag4.textContent = cards[0].tag4;
+  modallive.href = cards[0].liveDemoLink;
+  modalgithub.href = cards[0].githubLink;
 };
 
 btn2.onclick = function () {
-  modal2.style.display = 'block';
-  modal2Title.textContent = card2.title;
-  modal2Desc.textContent = card2.description;
-  modal2Img.src = card2.img;
-  modal2tag1.textContent = card2.tag1;
-  modal2tag2.textContent = card2.tag2;
-  modal2tag3.textContent = card2.tag3;
-  modal2tag4.textContent = card2.tag4;
-  modal2live.href = card2.liveDemoLink;
-  modal2github.href = card2.githubLink;
+  modal.style.display = 'block';
+  modalTitle.textContent = cards[1].title;
+  modalDesc.textContent = cards[1].description;
+  modalImg.src = cards[1].img;
+  modaltag1.textContent = cards[1].tag1;
+  modaltag2.textContent = cards[1].tag2;
+  modaltag3.textContent = cards[1].tag3;
+  modaltag4.textContent = cards[1].tag4;
+  modallive.href = cards[1].liveDemoLink;
+  modalgithub.href = cards[1].githubLink;
 };
 
 btn3.onclick = function () {
-  modal3.style.display = 'block';
-  modal3Title.textContent = card3.title;
-  modal3Desc.textContent = card3.description;
-  modal3Img.src = card3.img;
-  modal3tag1.textContent = card3.tag1;
-  modal3tag2.textContent = card3.tag2;
-  modal3tag3.textContent = card3.tag3;
-  modal3tag4.textContent = card3.tag4;
-  modal3live.href = card4.liveDemoLink;
-  modal3github.href = card4.githubLink;
+  modal.style.display = 'block';
+  modalTitle.textContent = cards[2].title;
+  modalDesc.textContent = cards[2].description;
+  modalImg.src = cards[2].img;
+  modaltag1.textContent = cards[2].tag1;
+  modaltag2.textContent = cards[2].tag2;
+  modaltag3.textContent = cards[2].tag3;
+  modaltag4.textContent = cards[2].tag4;
+  modallive.href = cards[3].liveDemoLink;
+  modalgithub.href = cards[3].githubLink;
 };
 
 btn4.onclick = function () {
-  modal4.style.display = 'block';
-  modal4Title.textContent = card4.title;
-  modal4Desc.textContent = card4.description;
-  modal4Img.src = card4.img;
-  modal4tag1.textContent = card4.tag1;
-  modal4tag2.textContent = card4.tag2;
-  modal4tag3.textContent = card4.tag3;
-  modal4tag4.textContent = card4.tag4;
-  modal4live.href = card4.liveDemoLink;
-  modal4github.href = card4.githubLink;
+  modal.style.display = 'block';
+  modalTitle.textContent = cards[3].title;
+  modalDesc.textContent = cards[3].description;
+  modalImg.src = cards[3].img;
+  modaltag1.textContent = cards[3].tag1;
+  modaltag2.textContent = cards[3].tag2;
+  modaltag3.textContent = cards[3].tag3;
+  modaltag4.textContent = cards[3].tag4;
+  modallive.href = cards[3].liveDemoLink;
+  modalgithub.href = cards[3].githubLink;
 };
 
 // When the user clicks on <span> (x), close the modal
-span1.onclick = function () {
-  modal1.style.display = 'none';
-};
-span2.onclick = function () {
-  modal2.style.display = 'none';
-};
-span3.onclick = function () {
-  modal3.style.display = 'none';
-};
-span4.onclick = function () {
-  modal4.style.display = 'none';
+span.onclick = function () {
+  modal.style.display = 'none';
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  if (event.target === modal1) {
-    modal1.style.display = 'none';
-  }
-  if (event.target === modal2) {
-    modal2.style.display = 'none';
-  }
-  if (event.target === modal3) {
-    modal3.style.display = 'none';
-  }
-  if (event.target === modal4) {
-    modal4.style.display = 'none';
+  if (event.target === modal) {
+    modal.style.display = 'none';
   }
 };
