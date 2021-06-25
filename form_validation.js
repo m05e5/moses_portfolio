e8b1d657ed8931710c148e2d84079909075078ac
m05e5 <calebmoses1111@gmail.com>
2021-06-25 09:26:33 +0100

    created form validation

--- form_validation.js
+++ form_validation.js
@@ -1,3 +1,9 @@
-// function isUpper(str) {
-//     return !/[a-z]/.test(str) && /[A-Z]/.test(str);
-// }
+
+const submitBtn = document.getElementById('submit');
+const message = document.getElementById('form-state');
+message.style.color = "red";
+submitBtn.addEventListener('click',function(event) {
+    const email = document.getElementById('email').value;
+    const lowerCaseEmail = email.toLowerCase();
+
+});
