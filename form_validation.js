1dad15fada2ecbcb504f683ec0c1a9248e792112
m05e5 <calebmoses1111@gmail.com>
2021-06-25 09:50:47 +0100

    fixed linters

--- form_validation.js
+++ form_validation.js
@@ -1,13 +1,11 @@
-
 const submitBtn = document.getElementById('submit');
 const message = document.getElementById('form-state');
-message.style.color = "red";
-submitBtn.addEventListener('click',function(event) {
-    const email = document.getElementById('email').value;
-    const lowerCaseEmail = email.toLowerCase();
-        
-if (email !== lowerCaseEmail) {
-    message.textContent = "Incorrect Email Format";
+message.style.color = 'red';
+submitBtn.addEventListener('click', function(event) {
+  const email = document.getElementById('email').value;
+  const lowerCaseEmail = email.toLowerCase();     
+  if (email !== lowerCaseEmail) {
+    message.textContent = 'Incorrect Email Format';
     event.preventDefault();
-}
+  }
 });
